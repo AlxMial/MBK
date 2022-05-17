968b7237deb4df17fd7bcb4c3c6d67e5ea1977db
PongpatWannarat <pongpat-w@hotmail.com>
2022-05-17 15:06:50 +0700

    Update contentLoader.js

--- client/src/components/Loadings/contentLoader.js
+++ client/src/components/Loadings/contentLoader.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import Spinner from './spinner/Spinner'
 
 const ContentLoader = () => {
     const { loading, message } = useSelector(({ common }) => common);
-
-    useEffect(() => {
-        console.log('loading: ', loading);
-    }, [loading]);
 
     return (
         <>
