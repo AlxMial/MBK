8b78559bf93b3e0c1e83e6e23903477b859dc62b
weatherblocker@gmail.com <weatherdance@hotmail.com>
2022-05-19 11:41:47 +0700

    commit

--- client/src/services/axiosUpload.js
+++ client/src/services/axiosUpload.js
@@ -4,12 +4,30 @@ const password = 'NFgrWHk2bTE1UURGZ0M0WXVwOVpxQT09'
 const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
 
 
-export default axios.create({
-  // baseURL: `http://undefined.ddns.net:8084/`,
+// export default axios.create({
+//   // baseURL: `http://undefined.ddns.net:8084/`,
+//   baseURL: `https://undefined.ddns.net/mbkimport/`,
+//   // baseURL: `http://localhost:3002/mbkimport/`,
+//   headers: {
+//     'Authorization': `Basic ${token}`,
+//     accessToken : localStorage.getItem("accessToken")
+//   }
+// });
+
+const axiosInstance = axios.create({
   baseURL: `https://undefined.ddns.net/mbkimport/`,
-  // baseURL: `http://localhost:3002/mbkimport/`,
-  headers: {
-    'Authorization': `Basic ${token}`,
-    accessToken : localStorage.getItem("accessToken")
-  }
+  // baseURL: `http://localhost:3001/mbkserver/`,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const tokens = token;
+    const auth = tokens ? `Bearer ${tokens}` : '';
+    config.headers.common['Authorization'] = auth;
+    config.headers.common['accessToken'] = sessionStorage.getItem("accessToken");;
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default axiosInstance;
