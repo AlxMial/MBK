6cd4ad9ea2d9e8ea093b5be1f7260a83674e1ba9
alongkon-nam <88099129+alongkon-nam@users.noreply.github.com>
2022-06-24 00:35:14 +0700

    report 1

--- client/src/layouts/Admin.js
+++ client/src/layouts/Admin.js
@@ -22,7 +22,7 @@ import ConditioRewardInfo from "views/admin/crm/conditionReward/conditioRewardIn
 import Stock from "views/admin/ecommerce/stock";
 import Order from "views/admin/ecommerce/order";
 import CancelAndReturn from "views/admin/ecommerce/cancelAndReturn";
-import ReportPointHistoryReport from "views/admin/report/PointHistoryReport";
+import PointHistoryReport from "views/admin/report/PointHistoryReport";
 import CollectPointsReport from "views/admin/report/CollectPointsReport";
 import CampaignRewardReport from "views/admin/report/CampaignRewardReport";
 import CampaignExchangeHistoryReport from "views/admin/report/CampaignExchangeHistoryReport";
@@ -76,10 +76,10 @@ export default function Admin() {
               <Route path="/admin/stocksinfo" exact component={StockInfo} />
               <Route path="/admin/stocksinfo/:id" exact component={StockInfo} />
               <Route path="/admin/points" exact component={PointManage} />
-              <Route path="/admin/pointhistoryreport" exact component={ReportPointHistoryReport} />
+              <Route path="/admin/allPointReport" exact component={PointHistoryReport} />
               <Route path="/admin/collectPointsReport" exact component={CollectPointsReport} />
-              <Route path="/admin/campaignRewardReport" exact component={CampaignRewardReport} />
-              <Route path="/admin/campaignExchangeHistoryReport" exact component={CampaignExchangeHistoryReport} />
+              <Route path="/admin/allRewardReport" exact component={CampaignRewardReport} />
+              <Route path="/admin/collectRewardReport" exact component={CampaignExchangeHistoryReport} />
               
               <Redirect from="/admin" to="/admin/users" />              
             </Switch>
